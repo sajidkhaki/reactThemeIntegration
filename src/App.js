c304156519308a1d4b02c840f976cefd4cd55208
sajidkhak <sajidkhaki@gmail.com>
2019-12-14 23:52:25 +0530

    implemented raect theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './header'
+import Menu from './menu'
+import Footer from './footer'
+import Table from './table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Menu />
+                <Table />
+                <Footer />
+            </div>
+        )
+    }
 }
-
-export default App;
